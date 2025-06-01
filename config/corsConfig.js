70caa00e4b15ae157615e96fa198e53bc264b6bb
Edward <edward.mokhabukhi@neutrinos.com>
2025-06-01 13:18:57 +0200

    changes

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -1,13 +1,5 @@
-const allowedOrigins = ['http://localhost:4200','http://localhost:63621','https://950d-41-122-208-13.ngrok-free.app'];
-
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: '*', // Allows requests from any origin
   methods: ['GET', 'POST', 'OPTIONS', 'DELETE', 'UPDATE', 'PUT'],
   allowedHeaders: ['Content-Type'],
   optionsSuccessStatus: 200,
